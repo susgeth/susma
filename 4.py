f72534022f208fff87843a76b4184bf2a9245f22
susgeth <37649743+susgeth@users.noreply.github.com>
2018-03-22 14:21:24 +0530

    4
    alphabet

---
+++ 4.py
@@ -0,0 +1,6 @@
+alpha=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+ch=input('enter a character')
+if ch in alpha:
+ print('alphabet')
+else:
+ print ('no')
