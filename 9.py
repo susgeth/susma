6c9b3b080427013ef632f8e30ed9827f5d292b97
susgeth <37649743+susgeth@users.noreply.github.com>
2018-03-23 14:05:37 +0530

    9
    seq

--- 9.py
+++ 9.py
@@ -1,12 +1,12 @@
-i=0
+n=0
 a=[]
 num1=int(input("Enter the array size:"))
 num2=int(input("How many first n digits u want to add?"))
 print("Enter number  in sequence")
-for i in range(0,num1):
+for n in range(0,num1):
     y=int(input())
     a.append(y)
 b=0
-for i in range(0,num2):
-    b+=a[i]
+for n in range(0,num2):
+    b+=a[n]
 print(b)
